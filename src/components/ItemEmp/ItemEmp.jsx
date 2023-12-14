20d6eb71d3362f29a09900191b5085ea6bf1110c
BlumaDeutsch <blimy1219@gmail.com>
2023-12-14 23:18:29 +0200

    changes

--- src/components/ItemEmp/ItemEmp.jsx
+++ src/components/ItemEmp/ItemEmp.jsx
@@ -5,24 +5,25 @@ import usersContext from '../../context/users';
 import './ItemEmp.css';
 
 // props
-export default function ItemEmp({user}) {
+export default function ItemEmp({ user }) {
     const [editMode, setEditMode] = useState(false)
     const { addToCart, onEdituser } = useContext(usersContext);
-    const toggleEditMode =()=>{
-        setEditMode((em) =>!em)
+    const toggleEditMode = () => {
+        setEditMode((em) => !em)
     }
-    const handleEditItemDetails = ({title, price, id})=>{
-        onEdituser({title, price, id})
+    const handleEditItemDetails = ({ title, price, id }) => {
+        onEdituser({ title, price, id })
         setEditMode(false)
     }
     return (
         <div id='user'>
+            <img src={user.picture.medium} alt="pic" />
             <h2> {user.name.title} </h2>
             <h2> {user.name.first} </h2>
-           {/* {editMode&&<ItemuserEdit editItemDetails={handleEditItemDetails} item={user}/>} 
-            <button onClick={toggleEditMode}><GrEdit/></button>
-            <button onClick={() => addToCart(user)}>Buy Now</button>
-            <img src={user.image} alt="" /> */}
+            <h4>phone: {user.phone}</h4>
+            <h4>email: {user.email}</h4>
+            <h4>nation: {user.nat}</h4>
+
         </div>
     );
 }
